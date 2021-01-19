60f631eb736f66ec90c01f3fc571ca8e819043b1
Adrian Furman <fadikk367@gmail.com>
2021-01-19 11:20:08 +0100

    Implement MeasurementOnlineService that implements MeasurementInterface

---
+++ src/services/MeasurementOnlineService.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+import { Measurement } from 'interfaces';
+import { MeasurementService } from 'interfaces/MeasurementService';
+
+
+class MeasurementOnlineService implements MeasurementService {
+  public async addOne(measurement: Measurement): Promise<Measurement> {
+    const headers = this.getAuthHeaders();
+
+    try {
+      const response = await axios.post<Measurement>('/measurements', measurement, { headers });
+      return response.data;
+    } catch(err) {
+      console.log(err);
+      throw new Error('not working');
+    }
+  }
+
+  public async fetchAll(): Promise<Measurement[]> {
+    const headers = this.getAuthHeaders();
+
+    try {
+      const response = await axios.get<Measurement[]>('/measurements', { headers });
+      return response.data;
+    } catch(err) {
+      console.log(err);
+      throw new Error('not working');
+    }
+  }
+
+  private getAuthHeaders(): { Authorization: string } {
+    return {
+      'Authorization': localStorage.getItem('token') || '',
+    }
+  }
+}
+
+export default new MeasurementOnlineService();
